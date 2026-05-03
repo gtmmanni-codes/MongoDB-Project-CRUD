64771f7b695c5a362d1a6fce7e681e37f4f229fc
gtmmanni-codes <gtmmanni@gmail.com>
2026-05-03 15:06:46 +0200

    inserting data

--- index.js
+++ index.js
@@ -4,22 +4,34 @@ let app = express();
 app.use(express.json());
 
 app.get("/student-read",async(req,res)=>{
-    res.send("Student read API");
+     let newDB = await dbConnection();
+    let newCollection = newDB.collection("Students");
+    let data = await newCollection.find().toArray();
+    let obj1 ={
+        status:1,
+        msg:"Data",data
+    }   
+    res.send({obj1 });
 });
 
 app.post("/student-insert",async(req,res)=>{
     let newDB = await dbConnection();
     let newCollection = newDB.collection("Students");
+    
     let obj={
         "sName":req.body.name,
         "sEmail":req.body.email
     }
+    let insertData = await newCollection.insertOne(obj)
+
+  
+
     
     console.log(obj);
-    
 
 
-    res.send("Student insert API");
+
+    res.send({obj});
 })
 
 app.listen("8000");
