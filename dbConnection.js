a5fb04fa0a9b7a094c0a3328215e70174dc381f3
gtmmanni-codes <gtmmanni@gmail.com>
2026-05-05 23:17:22 +0200

    comments

--- dbConnection.js
+++ dbConnection.js
@@ -4,6 +4,8 @@ const { MongoClient } = require('mongodb');
 
 // Database URL (local MongoDB server)
 const dataBaseURL = 'mongodb://localhost:27017';
+
+// Create a new MongoDB client instance
 const client = new MongoClient(dataBaseURL);
 
 let dbConnection = async()=>{
