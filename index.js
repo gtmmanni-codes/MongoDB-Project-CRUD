192b960c2da1e396fc411fe859708b5471cf351d
gtmmanni-codes <gtmmanni@gmail.com>
2026-05-20 23:50:20 +0200

    creating connection with mongoDB

--- index.js
+++ index.js
@@ -109,6 +109,7 @@ app.put("/student-update/:id", async (req, res) => {
 app.get("/student-search/:name",async(req,res)=>{
 
     let { name } = req.params;
+    let myDB = await dbConnection();
 })
 
 
